2eba0d929ed3352774ec4f0cef78947dd20ee995
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-26 08:44:59 +0100

    p

--- src/app/components/service/credit-card/credit-card.service.ts
+++ src/app/components/service/credit-card/credit-card.service.ts
@@ -7,7 +7,7 @@ import { CreditCard } from '../../../Models/credit-card';
   providedIn: 'root'
 })
 export class CreditCardService {
-  private baseUrl = 'api/credit-cards';
+  private baseUrl = '/api/credit-cards';
 
   constructor(private http: HttpClient) { }
 
